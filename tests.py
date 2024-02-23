4eca2f012cec1c2675e2023878fc788ea58bcae2
Maxime MARTIN <maxime.martin@st.com>
2024-02-23 13:06:20 +0100

    feat: add python tests

--- tests.py
+++ tests.py
@@ -14,84 +14,33 @@ class TestDbscan(TestCase):
         cls.dbscan.restype = ctypes.c_void_p
 
         cls.points = [
-            {
-                "X": 1,
-                "Y": 1,
-            },
-            {
-                "X": 2,
-                "Y": 2,
-            },
-            {
-                "X": 10,
-                "Y": 10,
-            },
-            {
-                "X": 43,
-                "Y": 43,
-            },
-            {
-                "X": 21,
-                "Y": 21,
-            },
-            {
-                "X": 3,
-                "Y": 3,
-            },
-            {
-                "X": 22,
-                "Y": 22,
-            },
-            {
-                "X": 40,
-                "Y": 40,
-            },
-            {
-                "X": 41,
-                "Y": 41,
-            },
-            {
-                "X": 20,
-                "Y": 20,
-            },
-            {
-                "X": 42,
-                "Y": 42,
-            },
-            {
-                "X": 100,
-                "Y": 100,
-            },
+            {"Vec": (6, 6)},
+            {"Vec": (1, 1)},
+            {"Vec": (2, 2)},
+            {"Vec": (10, 10)},
+            {"Vec": (43, 43)},
+            {"Vec": (21, 21)},
+            {"Vec": (3, 3)},
+            {"Vec": (22, 22)},
+            {"Vec": (40, 40)},
+            {"Vec": (41, 41)},
+            {"Vec": (20, 20)},
+            {"Vec": (42, 42)},
+            {"Vec": (100, 100)},
         ]
 
-    def test_dbscan(self):
+    def test_dbscan_brute(self):
         # GIVEN
         input = {
+            "algorithm": "brute",
             "epsilon": 5.0,
             "min_points": 3,
             "points": self.points,
         }
 
-        # WHEN
-        dbscan_output = self.dbscan(json.dumps(input).encode("utf-8"))
-        dbscan_output_bytes = ctypes.string_at(dbscan_output)
-        dbscan = json.loads(dbscan_output_bytes)
-
-        # THEN
-        self.assertEqual(
-            dbscan,
-            {
-                "labels": [1, 1, -1, 2, 3, 1, 3, 2, 2, 3, 2, -1],
-                "clusters": 3,
-            },
-        )
-
-    def test_dbscan_only_noise(self):
-        # GIVEN
-        input = {
-            "epsilon": 0.5,
-            "min_points": 3,
-            "points": self.points,
+        expected_output = {
+            "labels": [1, 1, 1, -1, 2, 3, 1, 3, 2, 2, 3, 2, -1],
+            "clusters": 3,
         }
 
         # WHEN
@@ -101,9 +50,81 @@ class TestDbscan(TestCase):
 
         # THEN
         self.assertEqual(
+            expected_output,
             dbscan,
-            {
-                "labels": [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
-                "clusters": 0,
-            },
+        )
+
+    def test_dbscan_brute_only_noise(self):
+        # GIVEN
+        input = {
+            "algorithm": "brute",
+            "epsilon": 0.5,
+            "min_points": 3,
+            "points": self.points,
+        }
+
+        expected_output = {
+            "labels": [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+            "clusters": 0,
+        }
+
+        # WHEN
+        dbscan_output = self.dbscan(json.dumps(input).encode("utf-8"))
+        dbscan_output_bytes = ctypes.string_at(dbscan_output)
+        dbscan = json.loads(dbscan_output_bytes)
+
+        # THEN
+        self.assertEqual(
+            expected_output,
+            dbscan,
+        )
+
+    def test_dbscan_kd_tree(self):
+        # GIVEN
+        input = {
+            "algorithm": "kd_tree",
+            "epsilon": 5.0,
+            "min_points": 3,
+            "points": self.points,
+        }
+
+        expected_output = {
+            "labels": [1, 1, 1, -1, 2, 3, 1, 3, 2, 2, 3, 2, -1],
+            "clusters": 3,
+        }
+
+        # WHEN
+        dbscan_output = self.dbscan(json.dumps(input).encode("utf-8"))
+        dbscan_output_bytes = ctypes.string_at(dbscan_output)
+        dbscan = json.loads(dbscan_output_bytes)
+
+        # THEN
+        self.assertEqual(
+            expected_output,
+            dbscan,
+        )
+
+    def test_dbscan_kd_tree_only_noise(self):
+        # GIVEN
+        input = {
+            "algorithm": "kd_tree",
+            "epsilon": 0.5,
+            "min_points": 3,
+            "points": self.points,
+        }
+
+        expected_output = {
+            "labels": [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+            "clusters": 0,
+        }
+
+        # WHEN
+        dbscan_output = self.dbscan(json.dumps(input).encode("utf-8"))
+        dbscan_output_bytes = ctypes.string_at(dbscan_output)
+        dbscan = json.loads(dbscan_output_bytes)
+
+        # THEN
+        self.assertEqual(
+            expected_output,
+            dbscan,
         )
